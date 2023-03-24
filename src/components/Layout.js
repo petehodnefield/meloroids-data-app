1e99f20831cde8201c0a28073a00c95307294e46
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-03-24 09:09:39 -0500

    added google font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 const Layout = ({ children }) => {
   return (
     <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <header>
         <Link href="/">
           <h1>Meloroids Data Application</h1>
